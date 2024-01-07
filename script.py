d328bbbc7f08574defadc1c0dc1d0f0535fcb152
ErykDarnowski <github@erykdarnowski.pl>
2024-01-07 12:35:25 +0100

    Fix issue with Polish diacritic characters

--- script.py
+++ script.py
@@ -12,4 +12,12 @@ with open('out.txt', 'w') as out_file:
         # go through every page
         for page in reader.pages:
             # save extracted text to output file
-            out_file.write(page.extract_text())
+            out_file.write(page.extract_text()
+                           .replace(' ą', 'ą')
+                           .replace(' ę', 'ę')
+                           .replace(' ń', 'ń')
+                           .replace(' ś', 'ś')
+                           .replace('Ŝ', 'ż'))
+                           # ^ fixes issue with Polish diacritic characters
+                           # uppercase characters are not done as I couldn't
+                           # find any issues with them as of now
